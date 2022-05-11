383588d11476a831e3919b3662aa4b83538cadcf
anshoeman <anshumannehru2002@gmail.com>
2022-05-11 23:23:52 +0530

    Check-Layout

---
+++ Components/Header.js
@@ -0,0 +1,10 @@
+import { View, Text, TouchableOpacity, SafeAreaView } from "react-native";
+import React from "react";
+import { Header as HeaderRNE, HeaderProps, Icon } from '@rneui/themed';
+export default function Header() {
+  return (
+    <View>
+      
+    </View>
+  );
+}
--- screens/Authenticated.js
+++ screens/Authenticated.js
@@ -1,39 +1,57 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
-import auth from '@react-native-firebase/auth';
-
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+import auth from "@react-native-firebase/auth";
+import Header from "../Components/Header";
 export default function Authenticated() {
+  const currentHour = new Date().getHours();
   const user = auth().currentUser;
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>You're Logged In</Text>
+    <View style={styles.Headers}>
+      <Header />
       <Image source={{ uri: user?.photoURL }} style={styles.image} />
-      <Text style={styles.text}>{user?.displayName}</Text>
-      <Text style={styles.text}>{user?.email}</Text>
-      <View style={{ marginTop: 30 }}>
-        <Button title="Signout" onPress={() => auth().signOut()} />
+      <View style={styles.HeadersText}>
+        <Text style={styles.text}>Welcome {user?.displayName}</Text>
+        <Text style={{fontWeight:"200"}}>
+          We Wish You A Hearty{" "}
+          {currentHour >= 12 && currentHour <= 17
+            ? "Good Afternoon!"
+            : currentHour >= 0 && currentHour < 12
+            ? "Good Morning!"
+            : "Good Evening!"}
+        </Text>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  Headers: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  HeadersText: {
+    marginLeft: 17,
+  },
   screen: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   title: {
-    fontSize: 25,
+    fontSize: 12,
     marginBottom: 30,
   },
   image: {
-    height: 150,
-    width: 150,
+    height: 70,
+    width: 70,
+    marginLeft: 17,
     borderRadius: 150,
-    marginBottom: 20,
+    marginBottom: 12,
+    marginTop: 10,
   },
   text: {
     fontSize: 20,
+    fontFamily: "Radio Canada, sans-serif",
+    fontWeight: "bold",
   },
 });
