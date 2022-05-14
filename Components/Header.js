59bb56bc9d79030170896ac1f96c06908fad623b
RoseVZ <priyarosev949@gmail.com>
2022-05-14 23:57:34 +0530

    n

---
+++ Components/Header.js
@@ -0,0 +1,60 @@
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+  StyleSheet,
+  Pressable,
+} from "react-native";
+import {
+  responsiveHeight,
+  responsiveWidth,
+  responsiveFontSize
+} from "react-native-responsive-dimensions";
+
+import React from "react";
+export default function Header() {
+  return (
+    <View style={styles.Header}>
+      <Pressable>
+        <TouchableOpacity>
+          <Text style={styles.Logout}>Logout</Text>
+        </TouchableOpacity>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  Header: {
+    position: "absolute",
+    width: responsiveWidth(100) ,
+    top: -1,
+    left: 0,
+    height: 59,
+    backgroundColor: "#F2EFE9",
+  },
+  Hamburger: {
+    position: "absolute",
+    width: 48,
+    height: 44,
+    left: 8,
+    top: 5,
+  },
+  Logout: {
+    position: "absolute",
+    width: responsiveWidth(45),
+    height: 26,
+    left: 272,
+    top: 14,
+    fontFamily: "Red Hat Display",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: 18,
+    lineHeight: 28,
+    display: "flex",
+    alignItems: "center",
+    textAlign: "center",
+    color: "#000000",
+  },
+});
