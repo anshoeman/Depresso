1bf6d396326f257372d65a66865f7c1f88fa1c9d
RoseVZ <priyarosev949@gmail.com>
2022-05-25 00:28:29 +0530

    flask

---
+++ backend/server.py
@@ -0,0 +1,82 @@
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+import h11
+from sklearn.metrics.pairwise import cosine_similarity  
+import pickle
+import numpy as np
+import pandas as pd
+from flask.views import MethodView
+from ast import literal_eval
+from sklearn.feature_extraction.text import CountVectorizer
+import jsonify
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+api=Api(app)
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('query')
+
+dataset=pd.read_csv('datacomp2 2.3.csv')
+
+
+count = CountVectorizer(stop_words=None)
+
+
+def clean_data(x):
+      
+    if isinstance(x, str):
+        return str.lower(x.replace(",", " "))
+    else:
+        return ''
+
+features = ['Genres']
+
+for feature in features:
+    dataset[feature] = dataset[feature].apply(clean_data)
+
+def soup(x):
+    return x['Genres'] + ' ' + x['Artist']
+dataset['soup'] = dataset.apply(soup, axis=1)
+matrix=count.fit_transform(dataset['soup'])
+cosine_sim2 = cosine_similarity(matrix, matrix)
+
+
+
+
+def find_title_from_index(index):
+    return dataset[dataset.index == index]["aTitle"].values[0]
+def get_recommendations(title, cosine_sim=cosine_sim2):
+        if title not in dataset['aTitle'].unique():
+            return "Not in Database"
+        else:
+            i=dataset.loc[dataset['aTitle']== title].index[0]
+            lst = list(enumerate(cosine_sim[i]))
+            lst= sorted(lst,key=lambda x:x[1],reverse=True)
+            lst = lst[1:6]
+            songindex = [i[0] for i in lst] #songs index
+            return dataset['aTitle'].iloc[songindex], songindex           
+        
+
+class Recommendation(Resource):
+    def get(self):
+        args = parser.parse_args()
+        user_query = args['query']
+        
+        
+        pred,index = get_recommendations("Sucker")
+        print(pred)
+        #json with list of songs(indices)
+        output = {"prediction": index,
+        
+        }
+        return output
+
+
+
+api.add_resource(Recommendation, '/','/x')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- screens/Authenticated.js
+++ screens/Authenticated.js
@@ -1,18 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View, Image, Button,ScrollView } from "react-native";
 import auth from "@react-native-firebase/auth";
 import Profile from "../Components/Layouts/Profile";
 import HeaderBar from "../Components/Layouts/Header";
+import axios from "axios";
 
 export default function Authenticated({ navigation }) {
   const user = auth().currentUser;
   const singOut = () => {
     auth().signOut();
   };
+  const [data, setData] = useState({
+    predict: " ",
+  });
+  function getData() {
+    axios({
+      method: "GET",
+      url: "/x",
+    })
+      .then((response) => {
+        const res = response.data
+        setData(({
+          predict: res.prediction,
+        
+        }))
+        console.log(predict);
+      }).catch((error) => {
+        if (error.response) {
+          console.log(error.response)
+          console.log(error.response.status)
+          console.log(error.response.headers)
+        }
+    })
+  }
+  // useEffect(() => {
+  //   fetch("/x").then((res) =>
+  //     res.json().then((data) => {
+  //       setData({
+  //         predict: data.prediction,
+  //       });
+  //     })
+    
+  //   );
+  // }, []);
+  getData
   return (
     <ScrollView>
       <HeaderBar logout={singOut} user={user} />
       <Profile user={user} navigation={navigation} />
+      <Text>{data.predict}</Text>
     </ScrollView>
+    
   );
 }
