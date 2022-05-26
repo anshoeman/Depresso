2e9aeec32b82f98bb1d6a17c4005ce6d2d97af74
anshoeman <anshumannehru2002@gmail.com>
2022-05-26 08:17:25 +0530

    Chart-Implementation

--- screens/Authenticated.js
+++ screens/Authenticated.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Image, Button,ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  ScrollView,
+} from "react-native";
 import auth from "@react-native-firebase/auth";
 import Profile from "../Components/Layouts/Profile";
 import HeaderBar from "../Components/Layouts/Header";
