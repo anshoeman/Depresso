8cdd8fe1084b9182e60582c5b978b60ae6ea37ba
anshoeman <anshumannehru2002@gmail.com>
2022-06-07 19:16:34 +0530

    flask integration cleanup

--- screens/Authenticated.js
+++ screens/Authenticated.js
@@ -1,3 +1,4 @@
+// //do adb reverse tcp:5000 tcp:5000(only if it still doesnt work)....
 import {
   StyleSheet,
   FlatList,
@@ -11,33 +12,13 @@ import React, { useState, useEffect } from "react";
 import auth from "@react-native-firebase/auth";
 import Profile from "../Components/Layouts/Profile";
 import HeaderBar from "../Components/Layouts/Header";
-/*Middleware and save the user API */
+
 export default function Authenticated({ navigation }) {
-  /*
-    const[state,setState]=useState()
-  */
   const user = auth().currentUser;
   const singOut = () => {
     auth().signOut();
   };
-  // const [data, setData] = useState({
-  //   predict: " ",
-  // });
 
-  // //do adb reverse tcp:5000 tcp:5000(only if it still doesnt work)....
-  // useEffect(() => {
-  //   fetch("http://127.0.0.1:5000/").then((res) =>
-  //     res.json().then((data) => {
-  //       console.log(data);
-  //       setData({
-  //         predict1: data.prediction1,
-  //         predict2: data.prediction2,
-  //         //array of index is in form of a string, not able to separate the values so manual fetch for each prediction
-  //       });
-  //     })
-  //   );
-  // }, []);
-  
   return (
     <ScrollView>
       <HeaderBar logout={singOut} user={user} />
