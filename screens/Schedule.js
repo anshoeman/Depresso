2eddad6aaab92a7da884f8aa272153792a2e1a9e
anshoeman <anshumannehru2002@gmail.com>
2022-06-12 02:57:21 +0530

    play-pause implementation and Refresh-Control

--- screens/Schedule.js
+++ screens/Schedule.js
@@ -1,22 +1,35 @@
 import React, { useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import Sound from "react-native-sound";
-
+import { Card, Button } from "react-native-elements";
 Sound.setCategory("Playback");
-
+/*get scheduled song name with API*/
 const Schedule = () => {
-  const [music, setMusic] = useState(null);
   const ScheduledSong = new Sound("song_1.mp3", Sound.MAIN_BUNDLE, (error) => {
     if (error) {
       console.log(error);
       return;
     }
-    console.log(ScheduledSong.getDuration());
   });
-
+  const [music, setMusic] = useState(ScheduledSong);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const MusicSessionHandler = () => {
+    console.log(streak, "Schedule Song" + music);
+    let currentState = isPlaying;
+    if (currentState) music.pause();
+    else music.play();
+    setIsPlaying(!isPlaying);
+  };
+  console.log(ScheduledSong.getDuration());
   return (
     <View>
-      <Text>Music</Text>
+      <Card>
+        <Card.Title>DANCE MONKEY</Card.Title>
+        <Button
+          title={isPlaying ? "Pause" : "Play"}
+          onPress={MusicSessionHandler}
+        />
+      </Card>
     </View>
   );
 };
